7224401aeb37e8740cefa6c4efa6ad3ddf8708a8
RossKieser <rosskieser@outlook.com>
2023-11-14 11:56:07 -0700

    added print statement to show that data is being requested

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def get_random_trivia():
     response = requests.get("http://localhost:5000/random_trivia")
 
     if response.status_code == 200:
+        print("Requesting Random Trivia")
         trivia = response.json().get("trivia")
         return trivia
     else:
